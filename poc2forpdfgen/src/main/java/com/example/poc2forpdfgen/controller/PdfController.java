45edb4e3a5f0b0ecfc6fff579fb97025a7b45f7c
manojpixid <manoj@pixid.ai>
2025-04-07 12:44:29 +0530

    added signature code for pdf generation

--- poc2forpdfgen/src/main/java/com/example/poc2forpdfgen/controller/PdfController.java
+++ poc2forpdfgen/src/main/java/com/example/poc2forpdfgen/controller/PdfController.java
@@ -10,11 +10,10 @@ import org.thymeleaf.context.Context;
 
 import com.example.poc2forpdfgen.service.PdfGeneratorService;
 
-
-
 @RestController
 @RequestMapping("/pdf")
 public class PdfController {
+
     private final PdfGeneratorService pdfGeneratorService;
 
     public PdfController(PdfGeneratorService pdfGeneratorService) {
@@ -24,11 +23,28 @@ public class PdfController {
     @GetMapping("/generate")
     public ResponseEntity<byte[]> generatePdf() {
         Context context = new Context();
-        context.setVariable("name", "John Doe");
-        context.setVariable("amount", "Australia");
-        context.setVariable("date", "2024-04-04");
-        context.setVariable("businessname", "PIXID AI");
-         context.setVariable("email", "pixid.ai@mail.com");
+        context.setVariable("date", "06/04/2025");
+        context.setVariable("firstName", "Pixid");
+        context.setVariable("lastName", "Ai");
+        context.setVariable("businessName", "PIXID AI");
+
+        context.setVariable("abn", "123232233232");
+        context.setVariable("physicalAddress", "Australia");
+        context.setVariable("contactPhoneNo", "123456789");
+        context.setVariable("emailAddress", "pixid.ai@mail.com");
+        context.setVariable("endDate", "1 Year from the date of this agreement");
+        context.setVariable("serviceFee", "A fee equal to 8% of the invoice value plus GST for a service provided by the\r\n"
+                + //
+                "Supplier to a customer under a booking processed using the Application will be\r\n"
+                + //
+                "payable. This fee will be capped and not exceed $50 plus GST per individual job\r\n"
+                + //
+                "processed.");
+        context.setVariable("RtbOwnerSign", "https://i.pinimg.com/474x/67/0c/e5/670ce5fcf05472b52600e21c84c94884.jpg");
+
+        context.setVariable("OwnerOrDirectorSign", "https://upload.wikimedia.org/wikipedia/commons/4/4e/MS_Dhoni_signature.png");
+
+        context.setVariable("DirectorOrSecretarySign", "https://upload.wikimedia.org/wikipedia/commons/4/4e/MS_Dhoni_signature.png");
 
         byte[] pdf = pdfGeneratorService.generatePdf("contract", context);
 
